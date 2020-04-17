ef8bd84ab3b7241edeb0b0c7e616a40d8e708e03
Marco <marco.faltoni@outlook.it>
2020-04-17 15:22:59 +0200

    Esercizio fatto

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // chiedo all'utente qual è il suo nome
-var nome_utente = prompt('Qual è il tuo nome?');
+var nome_utente = prompt('Qual è il tuo nome? (non inserire il cognome)');
 
 // verifico di aver letto l'input dell'utente stampando in console il contenuto del variabile
 console.log(nome_utente);
@@ -18,7 +18,9 @@ var colore_utente = prompt('Qual è il tuo colore preferito?');
 // verifico di aver letto l'input dell'utente stampando in console il contenuto del variabile
 console.log(colore_utente);
 
+
 // concateno i vari valori ottenuti e li visualizzo in pagina e in console
-var password_generata = nome_utente + cognome_utente + colore_utente + '20'
+var password_generata = nome_utente + cognome_utente + colore_utente + '20';
+
 // inserisco nello span che ho predisposto nell'html il nome dell'utente
 document.getElementById('password').innerHTML = password_generata;
